fb881340a1d73eba8895d308e559949042fb8ae7
Daniel Strivelli <Dan@n.fios-router.home>
2016-04-28 22:20:43 -0400

    Settings Comments

--- settingsPanel.java
+++ settingsPanel.java
@@ -10,38 +10,43 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.*;
 
 
-
+// Class that represents the settings panel within the game
+// This panel will be responsible for taking in all user
+// inputs before reaching the gameplay screen
 public class settingsPanel extends JPanel implements ActionListener
 {
+    // Instantiation of panel elements
     JTextField rn;
-    
     JLabel labelRealName, labelUserName, labelSpeedSlider;
     JButton settingsButton;
     JSlider setSpeed;
     
+    // Name is user property to be inputted and moved into the game panel
     String realName;
     
     int speed = 1;
-    
     int next = 0;
+
+    // Constructor for this panel
     public settingsPanel(){
         
         super();
         
+        // Set the layout for the panel to box layout
+        // which will fix elements to an axis
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         
+        // Set various panel/component attributes 
         setBackground(Color.BLUE);
-        
 
         JLabel title = new JLabel("Game Settings");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
         title.setFont(new Font("Arial", Font.BOLD, 75));
         title.setForeground(Color.white);
-        //title.setBounds(190,-50,300,200);
         add(title);
 
 
-// Add fields for the user
+        // Add fields for the user
         labelRealName = new JLabel("Name");
         labelRealName.setAlignmentX(Component.CENTER_ALIGNMENT);
         labelRealName.setForeground(Color.white);
@@ -77,8 +82,13 @@ public class settingsPanel extends JPanel implements ActionListener
         
 
     }
-    
+
+    // Method to get name variable from settings page
+    // Will be utilized by the card panel to move name to 
+    // gameplay panel and will eventually be used for XML
     public String getName() {
+        // If no name is given ("" in textfield)
+        // return John Doe as name
         if(Objects.equals(null, rn.getText()))
             realName = "John Doe";
         else 
@@ -86,21 +96,23 @@ public class settingsPanel extends JPanel implements ActionListener
         return(realName);
     }
   
+    // Accessor for speed of timer
     public float getSpeed() {
         return(speed);
     
     }
+
+    // Method that gets called when button gets clicked
+    // This method will set the name variable accordingly
+    // and prepare us to transition to the gameplay card
     public void actionPerformed(ActionEvent e) {
         // linewidth taken from the slider
         Object obj = e.getSource();
         if (obj == settingsButton) {
             realName = rn.getText();
-            speed = setSpeed.getValue() * 1000;
-            System.out.println("Settings Saved: " + realName + " " + speed);
-            
+            speed = setSpeed.getValue() * 1000;            
         }
-        
-
+    
     }
 }
 
